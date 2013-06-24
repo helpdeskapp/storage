55c0e3c45390986b3e8eed2d07379fdacfa34085
tab <tukhfatullin@gmail.com>
2013-06-24 12:25:05 +0700

    Change deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
 require 'openteam/capistrano/recipes'
 
 set :shared_children, fetch(:shared_children) + %w[.files tmp/cache]
+set :repository,  'git@bitbucket.org:tukhfatullin/storage.git'
+set :default_stage, 'do1'
