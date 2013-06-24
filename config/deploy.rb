92d871d5a7548e1af8781e676e013df4a0eb4713
tab <tukhfatullin@gmail.com>
2013-06-24 13:34:38 +0700

    Symlink newrelic config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require 'openteam/capistrano/recipes'
 
-set :shared_children, fetch(:shared_children) + %w[.files tmp/cache]
+set :shared_children, fetch(:shared_children) + %w[.files tmp/cache config/newrelic.yml]
 set :repository,  'git@bitbucket.org:tukhfatullin/storage.git'
 set :default_stage, 'do1'
