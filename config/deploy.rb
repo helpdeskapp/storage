6469a6717f1797dd607d4903a31f2d0cdc3094b9
tab <tukhfatullin@gmail.com>
2015-08-30 22:30:21 +0600

    fix repository url

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 require 'openteam/capistrano/recipes'
 
 set :shared_children, fetch(:shared_children) + %w[.files tmp/cache config/newrelic.yml]
-set :repository,  'git@github.com:helpdeskapp/storage.git'
+set :repository,  'https://github.com/helpdeskapp/storage.git'
 set :default_stage, 'docker'
